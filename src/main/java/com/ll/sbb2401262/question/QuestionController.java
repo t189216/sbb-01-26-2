f3cedcf40d162f52bc82694a8e37a02b6ba40b32
seoyeon <tus0926@naver.com>
2024-01-26 14:30:54 +0900

    2-10

--- src/main/java/com/ll/sbb2401262/question/QuestionController.java
+++ src/main/java/com/ll/sbb2401262/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,5 +20,13 @@ public class QuestionController {
         model.addAttribute("questionList", questionList);
 
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+
+        return "question_detail";
     }
 }
--- src/main/java/com/ll/sbb2401262/question/QuestionService.java
+++ src/main/java/com/ll/sbb2401262/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.ll.sbb2401262.question;
 
+import com.ll.sbb2401262.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,14 @@ public class QuestionService {
     public List<Question> getList() {
 
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
