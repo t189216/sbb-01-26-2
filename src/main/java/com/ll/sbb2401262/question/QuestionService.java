32611c425d805929e72de5d84e6cab28e6b8c22d
seoyeon <tus0926@naver.com>
2024-01-26 14:21:47 +0900

    2-09

---
+++ src/main/java/com/ll/sbb2401262/question/QuestionService.java
@@ -0,0 +1,18 @@
+package com.ll.sbb2401262.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+
+        return this.questionRepository.findAll();
+    }
+}
