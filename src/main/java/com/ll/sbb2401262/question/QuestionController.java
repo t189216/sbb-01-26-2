f95bb8876a41508833f3497075b99c857b298724
seoyeon <tus0926@naver.com>
2024-01-26 14:18:14 +0900

    2-07

---
+++ src/main/java/com/ll/sbb2401262/question/QuestionController.java
@@ -0,0 +1,23 @@
+package com.ll.sbb2401262.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+
+        return "question_list";
+    }
+}
