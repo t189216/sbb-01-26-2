15d0e6c1c398be8052457398a8895d405031ca7c
seoyeon <tus0926@naver.com>
2024-01-27 18:45:34 +0900

    3-09

--- src/main/java/com/ll/sbb2401262/domain/answer/AnswerService.java
+++ src/main/java/com/ll/sbb2401262/domain/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.ll.sbb2401262.domain.answer;
 
+import com.ll.sbb2401262.DataNotFoundException;
 import com.ll.sbb2401262.domain.question.Question;
 import com.ll.sbb2401262.domain.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +22,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
